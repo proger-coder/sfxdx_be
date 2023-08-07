d5e28489d985e67174e91d54c6a695df1c033e44
Ayrat <DIMANVAZ04@GMAIL.COM>
2023-08-07 10:29:36 +0300

    feat swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const port = 2023;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true })); // валидируем DTO-шки
-  await app.listen(3000, () => console.log('Sfxdx is listening on port 3000!'));
+
+  const options = new DocumentBuilder()
+    .setTitle('SFXDX API')
+    .setDescription('Описание API')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document); // Здесь 'api/docs' - это путь, по которому будет доступна документация
+
+  await app.listen(port, () => console.log('Sfxdx is listening on port', port));
 }
 bootstrap();
